ad21f7cdb251210a31f032ac19f02f3b1891170e
Luke Ginger <rc0039889@student.riversidecollege.ac.uk>
2019-11-25 15:12:52 +0000

    pi and c

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -24,6 +24,16 @@ namespace Numbers
             richTextBox1.AppendText(d1.ToString("0.0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            double pi = 3.1415926533589793;
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            double c = 299792458;
+            richTextBox1.AppendText(c.ToString("N") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000000000000E") + "\n");
+            richTextBox1.AppendText(c.ToString("0.00E") + "\n");
+
+
 
 
         }
